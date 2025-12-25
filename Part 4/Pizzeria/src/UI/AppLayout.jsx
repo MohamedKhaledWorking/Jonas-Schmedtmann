0efb1bc63a44f8625b1f1f837de995f49f24dcae
MohamedKhaledWorking <mohamed.khaled.mohamed.work@gmail.com>
2025-12-26 01:07:14 +0200

    added loading state to AppLayout; created Loading component for better user feedback during navigation

--- Part 4/Pizzeria/src/UI/AppLayout.jsx
+++ Part 4/Pizzeria/src/UI/AppLayout.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "./NavBar.jsx";
 import Footer from "./Footer.jsx";
+import Loading from "./Loading.jsx";
 
 export default function AppLayout() {
   const [isDark, setIsDark] = useState(true);
+  const { state } = useNavigation();
   return (
     <div
       className={`min-h-screen relative bg-mainBgc dark:bg-mainBgcDark text-textClr dark:text-textClrDark overflow-x-hidden ${
         isDark ? "dark" : ""
       }`}
     >
+      {state === "loading" && <Loading />}
       <NavBar setIsDark={setIsDark} />
       <Outlet />
       <Footer />
--- Part 4/Pizzeria/src/UI/Loading.jsx
+++ Part 4/Pizzeria/src/UI/Loading.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 
-export default function Loading({ label = "Baking Your Pizza" }) {
+export default function Loading({ label = "loading Your Pizza" }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="120"
-      height="120"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-pizza-icon lucide-pizza"
+    <div
+      className="
+    fixed inset-0 z-[9999999999]
+    flex items-center justify-center
+    bg-mainBgc/5 
+    backdrop-blur
+    text-3xl font-bold font-main text-white
+  "
     >
-      <path d="m12 14-1 1" class="svg-elem-1"></path>
-      <path d="m13.75 18.25-1.25 1.42" class="svg-elem-2"></path>
-      <path
-        d="M17.775 5.654a15.68 15.68 0 0 0-12.121 12.12"
-        class="svg-elem-3"
-      ></path>
-      <path d="M18.8 9.3a1 1 0 0 0 2.1 7.7" class="svg-elem-4"></path>
-      <path
-        d="M21.964 20.732a1 1 0 0 1-1.232 1.232l-18-5a1 1 0 0 1-.695-1.232A19.68 19.68 0 0 1 15.732 2.037a1 1 0 0 1 1.232.695z"
-        class="svg-elem-5"
-      ></path>
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="120"
+        height="120"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-pizza"
+      >
+        <path d="m12 14-1 1" className="svg-elem-1" />
+        <path d="m13.75 18.25-1.25 1.42" className="svg-elem-2" />
+        <path
+          d="M17.775 5.654a15.68 15.68 0 0 0-12.121 12.12"
+          className="svg-elem-3"
+        />
+        <path d="M18.8 9.3a1 1 0 0 0 2.1 7.7" className="svg-elem-4" />
+        <path
+          d="M21.964 20.732a1 1 0 0 1-1.232 1.232l-18-5a1 1 0 0 1-.695-1.232A19.68 19.68 0 0 1 15.732 2.037a1 1 0 0 1 1.232.695z"
+          className="svg-elem-5"
+        />
+      </svg>
+
+      <span className="ms-6 animate-pulse capitalize">{label}…</span>
+    </div>
   );
 }

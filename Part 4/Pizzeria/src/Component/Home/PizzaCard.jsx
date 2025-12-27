7978eb2c6e7cfe0b127135cf60852889c1e8deed
MohamedKhaledWorking <mohamed.khaled.mohamed.work@gmail.com>
2025-12-27 09:13:29 +0200

    refactor PizzaCard and TrendCard components to use Link for navigation

--- Part 4/Pizzeria/src/Component/Home/PizzaCard.jsx
+++ Part 4/Pizzeria/src/Component/Home/PizzaCard.jsx
@@ -1,69 +1,72 @@
 import { Clock, Star } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PizzaCard({ pizza }) {
   return (
     <>
-      <div className="w-full px-4 my-4 capitalize">
-        <div className="border border-textSecClr/40 dark:border-textSecClrDark/40 pb-8 rounded-4xl overflow-hidden group relative">
-          <p
-            className={`absolute left-5 top-5 px-4 rounded-full z-5 text-sm text-white ${
-              pizza?.isNew
-                ? "bg-emerald-400"
-                : pizza?.isPopular
-                ? "bg-orange-400"
-                : ""
-            }`}
-          >
-            {pizza?.isNew ? "New" : pizza?.isPopular ? "Popular" : ""}
-          </p>
-          <div
-            className={`absolute right-5 top-5 px-2 rounded-full z-5 text-sm bg-mainBgcDark dark:bg-mainBgc text-textClrDark dark:text-textClr font-semibold`}
-          >
-            <div className="flex items-center space-x-2">
-              <span>
-                <Star size={15} className="fill-yellow-500 text-yellow-500" />
-              </span>
-              <span>4.9</span>
-            </div>
-          </div>
-          <div className="w-full h-62.5 overflow-hidden relative">
-            <div className="absolute inset-0 bg-black/60 z-9 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 duration-300 ">
-              <button className="bg-orange-500 text-white py-2 px-4 rounded-full cursor-pointer ">
-                Order Now
-              </button>
-            </div>
-            <img
-              src={pizza?.image}
-              alt={pizza?.name + " image"}
-              loading="lazy"
-              className="w-full cursor-pointer group-hover:scale-150 duration-300"
-            />
-          </div>
-          <div className="px-4">
-            <div className="flex  justify-between items-center my-4">
-              <p className="font-bold font-main text-2xl line-clamp-1 hover:text-orange-500 duration-300 cursor-pointer ">
-                {pizza?.name}
-              </p>
-              <p>${pizza?.basePrice}</p>
-            </div>
-            <p className="text-textSecClr dark:text-textSecClrDark my-4 line-clamp-3 h-20">
-              {pizza?.description}
+      <Link to={`/pizza/${pizza?.id}`}>
+        <div className="w-full px-4 my-4 capitalize ">
+          <div className="border border-textSecClr/40 dark:border-textSecClrDark/40 pb-8 rounded-4xl overflow-hidden group relative">
+            <p
+              className={`absolute left-5 top-5 px-4 rounded-full z-5 text-sm text-white ${
+                pizza?.isNew
+                  ? "bg-emerald-400"
+                  : pizza?.isPopular
+                  ? "bg-orange-400"
+                  : ""
+              }`}
+            >
+              {pizza?.isNew ? "New" : pizza?.isPopular ? "Popular" : ""}
             </p>
-            <div className="flex items-center justify-between ">
+            <div
+              className={`absolute right-5 top-5 px-2 rounded-full z-5 text-sm bg-mainBgcDark dark:bg-mainBgc text-textClrDark dark:text-textClr font-semibold`}
+            >
               <div className="flex items-center space-x-2">
                 <span>
-                  <Clock />
+                  <Star size={15} className="fill-yellow-500 text-yellow-500" />
                 </span>
-                <span>
-                  {pizza?.cookingTime?.min} - {pizza?.cookingTime?.max} min
-                </span>
+                <span>4.9</span>
               </div>
-              <span>{pizza?.reviewCount} reviews</span>
+            </div>
+            <div className="w-full h-62.5 overflow-hidden relative">
+              <div className="absolute inset-0 bg-black/60 z-9 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 duration-300 ">
+                <button className="bg-orange-500 text-white py-2 px-4 rounded-full cursor-pointer ">
+                  Order Now
+                </button>
+              </div>
+              <img
+                src={pizza?.image}
+                alt={pizza?.name + " image"}
+                loading="lazy"
+                className="w-full cursor-pointer group-hover:scale-150 duration-300"
+              />
+            </div>
+            <div className="px-4">
+              <div className="flex  justify-between items-center my-4">
+                <p className="font-bold font-main text-2xl line-clamp-1 hover:text-orange-500 duration-300 cursor-pointer ">
+                  {pizza?.name}
+                </p>
+                <p>${pizza?.basePrice}</p>
+              </div>
+              <p className="text-textSecClr dark:text-textSecClrDark my-4 line-clamp-3 h-20">
+                {pizza?.description}
+              </p>
+              <div className="flex items-center justify-between ">
+                <div className="flex items-center space-x-2">
+                  <span>
+                    <Clock />
+                  </span>
+                  <span>
+                    {pizza?.cookingTime?.min} - {pizza?.cookingTime?.max} min
+                  </span>
+                </div>
+                <span>{pizza?.reviewCount} reviews</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
--- Part 4/Pizzeria/src/Component/Home/TrendCard.jsx
+++ Part 4/Pizzeria/src/Component/Home/TrendCard.jsx
@@ -1,58 +1,64 @@
 import { Star } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function TrendCard({ idx, pizza }) {
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 px-4 my-4">
-      <div className="rounded-2xl overflow-hidden group cursor-pointer border border-textSecClr/40 dark:border-textSecClrDark  ">
-        <div className="overflow-hidden h-96 ">
-          <img
-            src={pizza?.image}
-            alt={pizza?.name}
-            className="h-full w-full object-cover group-hover:scale-150 duration-300"
-          />
-        </div>
-        <div className="px-4 pb-8 ">
-          <p className="my-4 font-bold font-main text-3xl line-clamp-1 h-10 text-textClr dark:text-textClrDark group-hover:text-orange-500 duration-300 h-10 line-clamp-1">
-            {pizza?.name}
-          </p>
-          <div className="flex items-center justify-between my-4">
-            <div className="flex items-center space-x-2">
-              <span>
-                <Star size={20} className=" text-yellow-500 fill-yellow-500" />
-              </span>
-              <span className="font-bold">{pizza?.rating}</span>
-              <span className="text-textSecClr dark:text-textSecClrDark ">
-                ({pizza?.reviewCount} Reviews)
-              </span>
+      <Link to={`/pizza/${pizza?.id}`}>
+        <div className="rounded-2xl overflow-hidden group cursor-pointer border border-textSecClr/40 dark:border-textSecClrDark  ">
+          <div className="overflow-hidden h-96 ">
+            <img
+              src={pizza?.image}
+              alt={pizza?.name}
+              className="h-full w-full object-cover group-hover:scale-150 duration-300"
+            />
+          </div>
+          <div className="px-4 pb-8 ">
+            <p className="my-4 font-bold font-main text-3xl line-clamp-1 h-10 text-textClr dark:text-textClrDark group-hover:text-orange-500 duration-300 h-10 line-clamp-1">
+              {pizza?.name}
+            </p>
+            <div className="flex items-center justify-between my-4">
+              <div className="flex items-center space-x-2">
+                <span>
+                  <Star
+                    size={20}
+                    className=" text-yellow-500 fill-yellow-500"
+                  />
+                </span>
+                <span className="font-bold">{pizza?.rating}</span>
+                <span className="text-textSecClr dark:text-textSecClrDark ">
+                  ({pizza?.reviewCount} Reviews)
+                </span>
+              </div>
+              <div
+                className={`px-4 py-1 rounded-full flex text-sm ${
+                  idx === 1
+                    ? "bg-yellow-500  text-white"
+                    : idx == 2
+                    ? "bg-[#C4C4C4] text-black"
+                    : "bg-[#cd7f32] text-white"
+                } `}
+              >
+                #{idx}
+              </div>
             </div>
-            <div
-              className={`px-4 py-1 rounded-full flex text-sm ${
-                idx === 1
-                  ? "bg-yellow-500  text-white"
-                  : idx == 2
-                  ? "bg-[#C4C4C4] text-black"
-                  : "bg-[#cd7f32] text-white"
-              } `}
-            >
-              #{idx}
+            <div className="flex items-center justify-between ">
+              <div className="flex space-x-4 text-orange-500 ">
+                <p className="text-3xl font-bold font-main line-through ">
+                  $24.5
+                </p>
+                <p className="text-3xl font-bold font-main">
+                  ${pizza?.basePrice}
+                </p>
+              </div>
+              <button className="btn bg-orange-500 px-4 md:px-8 border-none shadow-none">
+                view details
+              </button>
             </div>
           </div>
-          <div className="flex items-center justify-between ">
-            <div className="flex space-x-4 text-orange-500 ">
-              <p className="text-3xl font-bold font-main line-through ">
-                $24.5
-              </p>
-              <p className="text-3xl font-bold font-main">
-                ${pizza?.basePrice}
-              </p>
-            </div>
-            <button className="btn bg-orange-500 px-4 md:px-8 border-none shadow-none">
-              view details
-            </button>
-          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }

b9a51b3735349bcdbc32bb3486c5e77e61f7edf2
MohamedKhaledWorking <mohamed.khaled.mohamed.work@gmail.com>
2026-01-06 15:48:45 +0200

    add HeroUI integration; configure Tailwind CSS and update App component for new provider

--- Part 4/dashBoard/src/App.jsx
+++ Part 4/dashBoard/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { HeroUIProvider } from "@heroui/react";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <HeroUIProvider>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </HeroUIProvider>
     </>
   );
 }
